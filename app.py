1dd033ed590f5d6dbc08501b2062cb12a621e80c
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-10-05 19:39:04 +0330

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def calc_result(score):
             if result == "Crack":
                 st.error("Crack Detected")
                 st.error(f"The life expectancy of this structure in {State} is {total_score}%")
-               #    for total_score in range (50,60)
-                              st.error("The Crack present type 1, and needs to be corrected as soon as possible.")
+                for total_score in range (50,60)
+                st.error("The Crack present type 1, and needs to be corrected as soon as possible.")
                    for total_score in range (25,35)
                               st.error("The Crack present type 2, and needs to be corrected as soon as possible.")
                    for total_score in range (35,45)
