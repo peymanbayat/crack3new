278e02bbabfd4c6e8a81950ac31e228abe60c91e
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-10-05 19:32:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,18 @@ def calc_result(score):
             if result == "Crack":
                 st.error("Crack Detected")
                 st.error(f"The life expectancy of this structure in {State} is {total_score}%")
-                st.error("The Crack present can be harmful, and needs to be corrected as soon as possible.")
+                   for total_score in range (0,25)
+                              st.error("The Crack present type 1, and needs to be corrected as soon as possible.")
+                   for total_score in range (25,35)
+                              st.error("The Crack present type 2, and needs to be corrected as soon as possible.")
+                   for total_score in range (35,45)
+                              st.error("The Crack present type 3, and needs to be corrected as soon as possible.")
+                   for total_score in range (45,55)
+                              st.error("The Crack present type 4, and needs to be corrected as soon as possible.")
+                   for total_score in range (55,65)
+                              st.error("The Crack present type 5, and needs to be corrected as soon as possible.")
+                   for total_score in range (65,100)
+                               st.error("The Crack present type 6, and needs to be corrected as soon as possible.")
 
    elif result == "Not-Crack":
        crack_score = random.randint(25, 40)
