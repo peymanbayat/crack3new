40cdc3b475b7e7dcc9692fa48231c467ba7949c2
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-11-10 22:02:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -105,13 +105,13 @@ def upload_and_predict(image_data,weight):
   st.sidebar.markdown("**Input Video**")
   st.sidebar.video(tffile.name)
   
-   global prediction
-   size = (224,224)
-   image = ImageOps.fit(image_data,size,Image.ANTIALIAS)
-   img  = np.array(image)
-   img_reshape = img[np.newaxis,...]
-   prediction = weight.predict(img_reshape)
-   return prediction
+  global prediction
+  size = (224,224)
+  image = ImageOps.fit(image_data,size,Image.ANTIALIAS)
+  img  = np.array(image)
+  img_reshape = img[np.newaxis,...]
+  prediction = weight.predict(img_reshape)
+  return prediction
 
 if file is  None:
    st.error("Please upload an image")
