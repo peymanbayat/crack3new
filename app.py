c4dd0d782785407e45b5360b095a558f28c94e02
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-11-10 23:02:04 +0330

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,11 @@ if file is  None:
    st.error("Please upload an image")
 else:   
    try:
-     image = video.open(file)
+     #image = image.open(file)
+     while vid.isOpened():
+        ret, frame = vid.read()
+        if not ret:
+            break
      rebuild_model = load_model("weights_data/my_model.h5")
      rebuild_model.compile(loss='categorical_crossentropy',
               optimizer='Adam',
